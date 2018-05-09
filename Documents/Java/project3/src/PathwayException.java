10c8f95fa724442687747cfa024a359c3a1665c2
augutierrez <35887905+augutierrez@users.noreply.github.com>
2018-05-09 12:34:54 -0700

    Delete PathwayException.java

--- Documents/Java/project3/src/PathwayException.java
+++
@@ -1,5 +0,0 @@
-public class PathwayException extends IllegalArgumentException {
-    public PathwayException() {}
-    public PathwayException(String message)
-    { super("Chess Piece(s) in the way!"); }
-}
